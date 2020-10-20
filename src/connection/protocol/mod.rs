209ae56131487bbb672be1b06ada94aba93fe517
Michał Sala <0havaker@gmail.com>
2020-10-20 12:41:32 +0200

    Test startup message exchange

--- src/connection/protocol/mod.rs
+++ src/connection/protocol/mod.rs
@@ -5,3 +5,25 @@ pub mod types;
 pub use request::Request;
 pub use response::Response;
 pub use types::StreamId;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::net::TcpStream;
+
+    #[test]
+    #[ignore]
+    fn test_startup_scylla() {
+        let startup_request = Request::Startup;
+
+        tokio_test::block_on(async {
+            let mut stream = TcpStream::connect("127.0.0.1:9042").await.unwrap();
+
+            let stream_id: StreamId = 1;
+            startup_request.write(stream_id, &mut stream).await.unwrap();
+            let (resp, received_stream_id) = Response::read(&mut stream).await.unwrap();
+            assert_eq!(resp, Response::Ready);
+            assert_eq!(received_stream_id, stream_id);
+        });
+    }
+}
