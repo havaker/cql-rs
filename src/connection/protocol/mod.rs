3be6b536b34b60e3658e933803c08b21638763b1
cvybhu <>
2020-10-20 11:07:47 +0200

    Clean up a bit

--- src/connection/protocol/mod.rs
+++ src/connection/protocol/mod.rs
@@ -1,10 +1,6 @@
-pub mod receiver;
-pub mod sender;
 pub mod request;
 pub mod response;
 pub mod types;
 
-pub use receiver::ProtocolReceiver;
-pub use sender::ProtocolSender;
 pub use request::Request;
 pub use response::Response;
