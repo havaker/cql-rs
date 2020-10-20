325a059b7c4a6d75e074012dd52d3df53ad396c3
Michał Sala <0havaker@gmail.com>
2020-10-20 16:01:58 +0200

    Example update

--- examples/simple.rs
+++ examples/simple.rs
@@ -9,7 +9,11 @@ use scylla::Query;
 async fn main() -> io::Result<()> {
     let conn = Connection::new("127.0.0.1:9042").await?;
 
-    let query = Query::new("hej");
-    conn.query(query).await.unwrap();
+    let query1 = Query::new("INSERT INTO ks.t(a,b,c) VALUES (1,2,'abc')");
+    let query2 = Query::new("INSERT INTO ks.t(a,b,c) VALUES (4,5,'def')");
+
+    conn.query(query1).await.unwrap();
+    conn.query(query2).await.unwrap();
+
     return Ok(());
 }
