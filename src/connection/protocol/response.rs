e629aa6d33080dda67443afa50bbeecb22a5ef52
Michał Sala <0havaker@gmail.com>
2020-10-20 10:29:49 +0200

    Response read skeleton

--- src/connection/protocol/response.rs
+++ src/connection/protocol/response.rs
@@ -1,23 +1,23 @@
+use tokio::io::AsyncReadExt;
+
 pub enum Response {
     Ready,
     Error,
     Result,
 }
 
-/*
-struct Response
+pub struct ResponseWithStreamID {
+    response: Response,
+    stream_id: u16,
+}
 
 impl Response {
-    pub async fn read<T: AsyncReadExt + Unpin>(reader: &mut T) -> Result<u16, std::io::Error> {
-        // TODO move writing header to another funciton
-        writer.write_u8(0x04).await?; // protocol version for request
-        writer.write_u8(0).await?; // no flags
-        writer.write_u16(stream_id).await?;
-        writer.write_u8(self.opcode()).await?;
-
-        let body = self.body();
-        writer.write_u32(body.len() as u32).await?;
-        writer.write_all(body.as_slice()).await
+    pub async fn read<T: AsyncReadExt + Unpin>(
+        reader: &mut T,
+    ) -> Result<ResponseWithStreamID, std::io::Error> {
+        return Ok(ResponseWithStreamID {
+            response: Response::Ready,
+            stream_id: 0,
+        });
     }
 }
-*/
